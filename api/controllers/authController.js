a2dbadb57804d514fb0dbc594f38acc5cb6bb155
vimlesh kumar <vimleshkumarvimlesh63@gmail.com>
2024-01-31 16:20:23 +0530

    create register api code

---
+++ api/controllers/authController.js
@@ -0,0 +1,51 @@
+import { hashPassword } from "../helper/authHelper.js";
+import userModel from "../models/userModel.js";
+
+export const signup = async(req, res)=>{
+    try{
+        const {name, email, password } = req.body;
+
+        //validations
+    if (!name) {
+      return res.send({ message: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+
+    //check user 
+    const existingUser = await userModel.findOne({email});
+    if(existingUser){
+         return res.status(200).send({
+            message:false,
+            message:"Already register please login"
+         })
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+        name,
+        email,
+        password:hashedPassword,
+    }).save();
+    res.status(200).send({
+        success:true,
+        message:"User Registered successfully",
+        user,
+    })
+        
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Error in registration ",
+            error,
+
+        })
+    }
+
+}
--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import authRoute from "./routes/authRoute.js"
+import connectDB from "../config/db.js";
+
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log("Mongodb is connected...");
-}).catch((err)=>{
-    console.log(err);
-})
+connectDB();
 
 
 const app  = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
