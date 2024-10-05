91f2f88e789241b3e663f31625c8ab2c77aa66c9
vimlesh kumar <vimleshkumarvimlesh63@gmail.com>
2024-10-05 14:07:30 +0530

    version

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import ThemeProvider from "./components/ThemeProvider.jsx";
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         secure: false,
       },
     },
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'redux-persist/integration/react' // Add this line to externalize the module
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+});
